6236c7340a33d5addadce049759cb0862caf22d9
paulmreese <paul.m.reese@gmail.com>
2023-10-25 13:33:46 -0400

    Account for merge of FreeRTOS smp branch into main

--- PicoHTTPServer/FreeRTOSConfig.h
+++ PicoHTTPServer/FreeRTOSConfig.h
@@ -102,13 +102,16 @@
 #define configMAX_API_CALL_INTERRUPT_PRIORITY   [dependent on processor and application]
 */
 
-#if FREE_RTOS_KERNEL_SMP // set by the RP2040 SMP port of FreeRTOS
+/* SMP Related config. */
+#define configUSE_PASSIVE_IDLE_HOOK             0
+#define portSUPPORT_SMP                         1
+
 /* SMP port only */
-#define configNUM_CORES                         2
+#define configNUMBER_OF_CORES                   2
 #define configTICK_CORE                         0
 #define configRUN_MULTIPLE_PRIORITIES           1
 #define configUSE_CORE_AFFINITY                 1
-#endif
+
 
 /* RP2040 specific */
 #define configSUPPORT_PICO_SYNC_INTEROP         1
